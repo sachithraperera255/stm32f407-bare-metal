eaab58df6b205b8377c7bde6bb7304d983e819ef
sachithraperera255 <sachithraperera255@gmail.com>
2026-05-09 23:57:13 -0400

    mapped RCC_AHB1ENR register using bit-field and structure

---
+++ LED_BitFieldToggle/Src/main.h
@@ -0,0 +1,48 @@
+/*
+ * main.h
+ *
+ *  Created on: May 6, 2026
+ *      Author: Sachithra
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+typedef struct
+{
+	uint32_t gpioa_en:		1;
+	uint32_t gpiob_en:		1;
+	uint32_t gpioc_en:		1;
+	uint32_t gpiod_en:		1;
+	uint32_t gpioe_en:		1;
+	uint32_t gpiof_en:		1;
+	uint32_t gpiog_en:		1;
+	uint32_t gpioh_en:		1;
+	uint32_t gpioi_en:		1;
+	uint32_t reserved1:		3;
+	uint32_t crc_en:		1;
+	uint32_t reserved2:		5;
+	uint32_t bkspsram_en:	1;
+	uint32_t reserved3:		1;
+	uint32_t ccmdataram_en:	1;
+	uint32_t dma1_en:		1;
+	uint32_t dma2_en:		1;
+	uint32_t reserved4:		2;
+	uint32_t ethmac_en:		1;
+	uint32_t ethmactx_en:	1;
+	uint32_t ethmacrx_en:	1;
+	uint32_t ethmacptp_en:	1;
+	uint32_t otghs_en:		1;
+	uint32_t otghsulpi_en:	1;
+	uint32_t resereved5:	1;
+
+}RCC_AHB1ENR;
+
+
+typedef struct
+{
+
+};
+
+
+#endif /* MAIN_H_ */
